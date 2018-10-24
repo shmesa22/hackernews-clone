01a4514b5ef63832e988917bbfde361d3a88789e
Simon Hoyos <shmesa23@gmail.com>
2018-10-24 05:34:24 -0500

    Set story reducer

---
+++ src/store/story/actions.js
@@ -0,0 +1,44 @@
+import hackerNewsApi from 'services/hackerNewsApi';
+
+export const actionTypes = {
+  FETCH_STORY_IDS_REQUEST: 'FETCH_STORY_IDS_REQUEST',
+  FETCH_STORY_IDS_SUCCESS: 'FETCH_STORY_IDS_SUCCESS',
+  FETCH_STORY_IDS_FAILURE: 'FETCH_STORY_IDS_FAILURE',
+  FETCH_STORIES_REQUEST: 'FETCH_STORY_REQUEST',
+  FETCH_STORIES_SUCCESS: 'FETCH_STORY_SUCCESS',
+  FETCH_STORIES_FAILURE: 'FETCH_STORY_FAILURE',
+};
+
+const action = (type, payload) => ({ type, payload });
+
+const actions = {
+  fetchStoryIds: (payload = {}) => {
+    return dispatch => {
+      dispatch(action(actionTypes.FETCH_STORY_IDS_REQUEST, payload));
+
+      return hackerNewsApi
+        .getTopStoryIds()
+        .then(storyIds => {
+          dispatch(action(actionTypes.FETCH_STORY_IDS_SUCCESS, { storyIds }));
+          dispatch(actions.fetchStories({ storyIds, page: 0 }));
+
+          return storyIds;
+        })
+        .catch(err => dispatch(action(actionTypes.FETCH_STORY_IDS_FAILURE, err)));
+    };
+  },
+  fetchStories: (payload = {}) => {
+    return dispatch => {
+      const { storyIds, page } = payload;
+
+      dispatch(action(actionTypes.FETCH_STORIES_REQUEST, payload));
+
+      return hackerNewsApi
+        .getStoriesByPage(storyIds, page)
+        .then(stories => dispatch(action(actionTypes.FETCH_STORIES_SUCCESS, { stories })))
+        .catch(err => dispatch(action(actionTypes.FETCH_STORIES_FAILURE, err)));
+    };
+  },
+};
+
+export default actions;
